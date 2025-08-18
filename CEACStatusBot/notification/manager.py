db03c5d5d787485cd728777964280f3436cfc90e
JiyangZhou <jiyangz23212@gmail.com>
2025-08-18 15:51:44 -0700

    fix: send notification logic fix, add active hour for config

--- CEACStatusBot/notification/manager.py
+++ CEACStatusBot/notification/manager.py
@@ -1,32 +1,61 @@
-import os
 import json
+import os
 from datetime import datetime
-from .handle import NotificationHandle
-from CEACStatusBot.request import query_status
-from CEACStatusBot.captcha import CaptchaHandle, OnnxCaptchaHandle
 
-class NotificationManager():
-    def __init__(self, location: str, number: str, passport_number: str, surname: str, captchaHandle: CaptchaHandle = OnnxCaptchaHandle("captcha.onnx")) -> None:
+from CEACStatusBot.captcha import CaptchaHandle, OnnxCaptchaHandle
+from CEACStatusBot.request import query_status
+
+from .handle import NotificationHandle
+
+DEFAULT_ACTIVE_HOURS = "00:00-23:59"
+
+
+class NotificationManager:
+    def __init__(
+        self,
+        location: str,
+        number: str,
+        passport_number: str,
+        surname: str,
+        captchaHandle: CaptchaHandle = OnnxCaptchaHandle("captcha.onnx"),
+    ) -> None:
         self.__handleList = []
         self.__location = location
         self.__number = number
         self.__captchaHandle = captchaHandle
         self.__passport_number = passport_number
         self.__surname = surname
-        self.__status_file = 'status_record.json'
+        self.__status_file = "status_record.json"
+
+    def _get_hour_range(self) -> list:
+        active_hours = os.getenv("ACTIVE_HOURS")
+        if active_hours is None:
+            active_hours = DEFAULT_ACTIVE_HOURS
+        start_str, end_str = active_hours.split("-")
+        start = datetime.datetime.strptime(start_str, "%H:%M").time()
+        end = datetime.datetime.strptime(end_str, "%H:%M").time()
+        if start > end:
+            raise ValueError("Start time must be before end time, got start: {start}, end: {end}")
+        return start, end
 
     def addHandle(self, notificationHandle: NotificationHandle) -> None:
         self.__handleList.append(notificationHandle)
 
     def send(self) -> None:
-        res = query_status(self.__location, self.__number, self.__passport_number, self.__surname, self.__captchaHandle)
-        current_status = res['status']
+        res = query_status(
+            self.__location,
+            self.__number,
+            self.__passport_number,
+            self.__surname,
+            self.__captchaHandle,
+        )
+        current_status = res["status"]
 
         # Load the previous statuses from the file
         statuses = self.__load_statuses()
 
         # Check if the current status is different from the last recorded status
-        if not statuses or current_status != statuses[-1]['status']:
+        if not statuses or current_status != statuses[-1]["status"]:
             self.__save_current_status(current_status)
             self.__send_notifications(res)
         else:
@@ -34,22 +63,25 @@ class NotificationManager():
 
     def __load_statuses(self) -> list:
         if os.path.exists(self.__status_file):
-            with open(self.__status_file, 'r') as file:
-                return json.load(file).get('statuses', [])
+            with open(self.__status_file, "r") as file:
+                return json.load(file).get("statuses", [])
         return []
 
     def __save_current_status(self, status: str) -> None:
         statuses = self.__load_statuses()
-        statuses.append({'status': status, 'date': datetime.now().isoformat()})
+        statuses.append({"status": status, "date": datetime.now().isoformat()})
 
-        with open(self.__status_file, 'w') as file:
-            json.dump({'statuses': statuses}, file)
+        with open(self.__status_file, "w") as file:
+            json.dump({"statuses": statuses}, file)
 
     def __send_notifications(self, res: dict) -> None:
-        if res['status'] == "Refused":
+        if res["status"] == "Refused":
             import pytz, datetime
+
             try:
                 TIMEZONE = os.environ["TIMEZONE"]
+                ACTIVE_HOURS = os.getenv("ACTIVE_HOURS", DEFAULT_ACTIVE_HOURS)
+
                 localTimeZone = pytz.timezone(TIMEZONE)
                 localTime = datetime.datetime.now(localTimeZone)
             except pytz.exceptions.UnknownTimeZoneError:
@@ -59,11 +91,11 @@ class NotificationManager():
                 print("TIMEZONE Error")
                 localTime = datetime.datetime.now()
 
-            if localTime.hour < 8 or localTime.hour > 22:
-                print("In Manager, no disturbing time")
-                return
-            if localTime.minute > 30:
-                print("In Manager, no disturbing time")
+            active_hour_start, active_hour_end = self._get_hour_range()
+            start_dt = datetime.datetime.combine(localTime.date(), active_hour_start, tzinfo=localTimeZone)
+            end_dt = datetime.datetime.combine(localTime.date(), active_hour_end, tzinfo=localTimeZone)
+            if not (start_dt <= localTime <= end_dt):
+                print(f"Outside active hours {self.__active_hours_range}")
                 return
 
         for notificationHandle in self.__handleList:
